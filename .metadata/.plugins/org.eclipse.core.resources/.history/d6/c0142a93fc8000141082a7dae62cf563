9e7c58c99cb222c1af0c01d95539d2a74c8d9f00
qkdtjdghk <bbongs98@naver.com>
2014-12-11 16:41:14 +0900

    14/12/11

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d6/c0142a93fc8000141082a7dae62cf563
@@ -0,0 +1,10 @@
+package chat;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/src/Chap13/ThreadInterruptEx.java
@@ -0,0 +1,70 @@
+package Chap13;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+class TimerRunnable implements Runnable {
+	JLabel timerLabel;
+
+	public TimerRunnable(JLabel timerLabel) {
+		this.timerLabel = timerLabel;
+	}
+
+	public void run() {
+		int n = 0;
+		while (true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+	}
+}
+
+public class ThreadInterruptEx extends JFrame {
+	Thread th;
+
+	public ThreadInterruptEx() {
+		setTitle("ThreadInterruptEx ¿¹Á¦");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC, 80));
+
+		TimerRunnable runnable = new TimerRunnable(timerLabel);
+		th = new Thread(runnable);
+		c.add(timerLabel);
+
+		JButton btn = new JButton("kill Timer");
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				th.interrupt();
+				JButton btn = (JButton) e.getSource();
+				btn.setEnabled(false);
+			}
+		});
+		c.add(btn);
+		setSize(300, 150);
+		setVisible(true);
+
+		th.start();
+	}
+
+	public static void main(String[] args) {
+		new ThreadInterruptEx();
+
+	}
+
+}
